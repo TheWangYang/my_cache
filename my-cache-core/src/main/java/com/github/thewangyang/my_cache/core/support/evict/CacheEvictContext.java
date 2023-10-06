bc45514f69811d7d1511e03d1cf25cc3ee9d9ec2
TheWangYang <wyysuccess2020@163.com>
2023-10-06 10:30:43 +0800

    Add CacheEvictContext.java

---
+++ my-cache-core/src/main/java/com/github/thewangyang/my_cache/core/support/evict/CacheEvictContext.java
@@ -0,0 +1,47 @@
+package com.github.thewangyang.my_cache.core.support.evict;
+
+
+import com.github.thewangyang.my_cache.api.ICache;
+import com.github.thewangyang.my_cache.api.ICacheEvictContext;
+
+//移除元素的上下文类
+public class CacheEvictContext<K, V> implements ICacheEvictContext<K, V> {
+
+    //新加的key
+    private K key;
+
+    //创建cache对象
+    private ICache<K, V> cache;
+
+    //最大的大小
+    private int size;
+
+    //返回移除淘汰类对象的方法
+    public CacheEvictContext<K, V> key(K key){
+        this.key = key;
+        return this;
+    }
+
+
+    //得到key
+    @Override
+    public K key() {
+        return key;
+    }
+
+    @Override
+    public ICache<K, V> cache() {
+        return cache;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    //根据size得到新的CacheEvictContext对象
+    public CacheEvictContext<K, V> size(int size){
+        this.size = size;
+        return this;
+    }
+}
