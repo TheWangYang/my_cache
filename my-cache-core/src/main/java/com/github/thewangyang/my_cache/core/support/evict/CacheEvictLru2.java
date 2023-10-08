37265747b3a8761aab170c529b59af9a4be6560e
TheWangYang <wyysuccess2020@163.com>
2023-10-08 15:56:47 +0800

    Add support/evict/CacheEvictLru2.java

--- my-cache-core/src/main/java/com/github/thewangyang/my_cache/core/support/evict/CacheEvictLru2.java
+++ my-cache-core/src/main/java/com/github/thewangyang/my_cache/core/support/evict/CacheEvictLru2.java
@@ -1,16 +1,22 @@
 package com.github.thewangyang.my_cache.core.support.evict;
 
 
-//定义淘汰策略-最近最少使用
-//在CacheEvictLru.java的基础上进行扩充
+
 
 import com.github.houbb.log.integration.core.Log;
 import com.github.houbb.log.integration.core.LogFactory;
+
+import com.github.thewangyang.my_cache.api.ICache;
 import com.github.thewangyang.my_cache.api.ICacheEntry;
 import com.github.thewangyang.my_cache.api.ICacheEvictContext;
+import com.github.thewangyang.my_cache.core.model.CacheEntry;
 import com.github.thewangyang.my_cache.core.support.struct.lru.ILruMap;
-import com.github.thewangyang.my_cache.core.support.struct.lru.impl.LruMapCircleList;
+import com.github.thewangyang.my_cache.core.support.struct.lru.impl.LruMapDoubleList;
 
+
+
+//定义淘汰策略-最近最少使用
+//在CacheEvictLru.java的基础上进行扩充
 public class CacheEvictLru2<K, V> extends AbstractCacheEvict<K, V> {
 
     //定义log对象
@@ -22,14 +28,70 @@ public class CacheEvictLru2<K, V> extends AbstractCacheEvict<K, V> {
     //定义第2次访问的lru
     private final ILruMap<K, V> secondLruMap;
 
-    //添加构造函数
+    //重写默认构造函数
     public CacheEvictLru2(){
-        this.firstLruMap = new L
+        //创建双向链表LruMap对象
+        this.firstLruMap = new LruMapDoubleList<>();
+        this.secondLruMap = new LruMapDoubleList<>();
     }
 
-    //定义
+
+    //重写淘汰策略函数
     @Override
     protected ICacheEntry<K, V> doEvict(ICacheEvictContext<K, V> context) {
-        return null;
+        ICacheEntry<K, V> result = null;
+        final ICache<K, V> cache = context.cache();
+
+        if(cache.size() >= context.size()){
+            //创建将淘汰的null空的entry对象
+            ICacheEntry<K, V> evictEntry = null;
+
+            //首先从firstMap中移除元素
+            if(!firstLruMap.isEmpty()){
+                evictEntry = this.firstLruMap.removeEldest();
+                log.debug("从 firstLruMap 中淘汰数据：{}", evictEntry);
+            }else{
+                evictEntry = this.secondLruMap.removeEldest();
+                log.debug("从 secondLruMap 中淘汰数据：{}", evictEntry);
+            }
+
+            //然后执行缓存移除操作
+            final K evictKey = evictEntry.key();
+            V evictValue = cache.remove(evictKey);
+            result = CacheEntry.of(evictKey, evictValue);
+        }
+
+        return result;
+    }
+
+
+    //重写updateKey函数
+    @Override
+    public void updateKey(K key) {
+        //判断是否在firstMap或secondMap中出现
+        if(firstLruMap.contains(key) || secondLruMap.contains(key)){
+            //删除key
+            this.removeKey(key);
+            //加入到secondLruMap中
+            secondLruMap.updateKey(key);
+
+            log.debug("key: {} 多次访问，加入到 secondLruMap 中", key);
+        }else{//不在两个lruMap中
+            firstLruMap.updateKey(key);
+            log.debug("key: {} 首次访问，加入到 firstLruMap 中", key);
+        }
+
+    }
+
+    @Override
+    public void removeKey(K key) {
+        //多次访问map删除逻辑
+        if(secondLruMap.contains(key)){
+            secondLruMap.removeKey(key);
+            log.debug("key: {} 从 secondLruMap 中移除", key);
+        }else{
+            firstLruMap.removeKey(key);
+            log.debug("key: {} 从 firstLruMap 中移除", key);
+        }
     }
 }
