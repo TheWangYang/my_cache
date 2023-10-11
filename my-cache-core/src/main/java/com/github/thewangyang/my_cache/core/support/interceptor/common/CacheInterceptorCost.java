94942fca309756dcf37043acfbcbf6afe4f6bf9c
TheWangYang <wyysuccess2020@163.com>
2023-10-11 21:24:43 +0800

    Add CacheSlowListenerContext.java

---
+++ my-cache-core/src/main/java/com/github/thewangyang/my_cache/core/support/interceptor/common/CacheInterceptorCost.java
@@ -0,0 +1,47 @@
+package com.github.thewangyang.my_cache.core.support.interceptor.common;
+
+
+import com.github.houbb.heaven.util.util.CollectionUtil;
+import com.github.houbb.log.integration.core.Log;
+import com.github.houbb.log.integration.core.LogFactory;
+import com.github.thewangyang.my_cache.api.ICacheInterceptor;
+import com.github.thewangyang.my_cache.api.ICacheInterceptorContext;
+import com.github.thewangyang.my_cache.api.ICacheSlowListener;
+import com.github.thewangyang.my_cache.core.support.listener.slow.CacheSlowListenerContext;
+
+import java.util.Collection;
+import java.util.List;
+
+// 定义耗时统计类
+// 统计
+public class CacheInterceptorCost<K, V> implements ICacheInterceptor<K, V> {
+    // 定义log对象
+    private static final Log log = LogFactory.getLog(CacheInterceptorCost.class);
+
+    @Override
+    public void before(ICacheInterceptorContext<K, V> context) {
+        log.debug("耗时开始, 方法名称为: {}", context.method().getName());
+    }
+
+    // after()方法中得到方法method()操作的耗时详情
+    @Override
+    public void after(ICacheInterceptorContext<K, V> context) {
+        // 得到耗时时间
+        long costMills = context.startMills() - context.endMills();
+
+        // 得到cache兑现执行的方法
+        String methodName = context.method().getName();
+
+        log.debug("耗时结束, 方法名称为: {}, 耗时时间: {}ms", methodName, costMills);
+
+        // 实现慢日志监听
+        // 获得context上下文对象的cache对象中的slowListeners监听器
+        List<ICacheSlowListener> slowListenerList = context.cache().slowListeners();
+
+        // 判断slowListenerList是否为空
+        if(CollectionUtil.isNotEmpty(slowListenerList)){
+            // 得到CacheSlowListenerContext
+            CacheSlowListenerContext cacheSlowListenerContext
+        }
+    }
+}
---
+++ my-cache-core/src/main/java/com/github/thewangyang/my_cache/core/support/listener/slow/CacheSlowListenerContext.java
@@ -0,0 +1,102 @@
+package com.github.thewangyang.my_cache.core.support.listener.slow;
+
+
+import com.github.thewangyang.my_cache.api.ICacheSlowListenerContext;
+import sun.misc.Cache;
+
+
+// 定义慢日志监听器
+// 实现ICacheSlowListenerContext接口
+public class CacheSlowListenerContext implements ICacheSlowListenerContext {
+
+    // 定义methodName
+    private String methodName;
+
+    // 定义参数信息
+    private Object[] params;
+
+    // 定义结果数组
+    private Object[] result;
+
+    // 定义开始时间
+    private long startTimeMills;
+
+    // 定义结束时间
+    private long endTimeMills;
+
+    // 定义耗时时间
+    private long costTimeMills;
+
+
+    // 创建静态类构造方法
+    public static CacheSlowListenerContext newInstance(){
+        return new CacheSlowListenerContext();
+    }
+
+    // 定义
+    @Override
+    public String methodName() {
+        return methodName;
+    }
+
+    // 返回类对象的methodName函数
+    public CacheSlowListenerContext methodName(String methodName){
+        this.methodName = methodName;
+        return this;
+    }
+
+
+    @Override
+    public Object[] params() {
+        return params;
+    }
+
+    public CacheSlowListenerContext params(Object[] params) {
+        this.params = params;
+        return this;
+    }
+
+
+    @Override
+    public Object[] result() {
+        return result;
+    }
+
+    public CacheSlowListenerContext params(Object[] result) {
+        this.result = result;
+        return this;
+    }
+
+    @Override
+    public long startTimeMills() {
+        return startTimeMills;
+    }
+
+    public CacheSlowListenerContext startTimeMills(long startTimeMills) {
+        this.startTimeMills = startTimeMills;
+        return this;
+    }
+
+
+    @Override
+    public long endTimeMills() {
+        return endTimeMills;
+    }
+
+    public CacheSlowListenerContext endTimeMills(long endTimeMills) {
+        this.endTimeMills = endTimeMills;
+        return this;
+    }
+
+
+    @Override
+    public long costTimeMills() {
+        return 0;
+    }
+
+    public CacheSlowListenerContext costTimeMills(long costTimeMills) {
+        this.costTimeMills = costTimeMills;
+        return this;
+    }
+
+}
